baee20c5ae8f69181cb1d9cadc920cc120007bc7
hannesGreenAI <h.schuetze@greentech.energy>
2021-12-16 15:18:05 +0100

    Planung Struktur

--- Alg_HanneskNN.py
+++ Alg_HanneskNN.py
@@ -1,5 +1,13 @@
 def train():
-    ..
+    # Input: Path of folder for training images and classification list
+    # Output:
+        # ISS = Image Standard Set of web cam images (rescaled) and with classification, maybe num. images is limited and choosen wisely
+        # k = the Optimal value for k for the classification
 
-def predict():
-        ..
+def test():
+    # Input
+        # ISS = The classification model (in the cas the ISS)
+    # Output:
+        # classification label
+    # Method: kNN, squared distance
+
