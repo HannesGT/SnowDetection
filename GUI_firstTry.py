312e1a55074f0d41b3fc65da55b9790a007f47d9
Danielapv99 <d.schumann@greentech.energy>
2022-01-13 16:42:08 +0100

    Button erstellt

--- GUI_firstTry.py
+++ GUI_firstTry.py
@@ -4,4 +4,7 @@ window = tkinter.Tk() # Erzeugt Fenster
 
 window.title('Sonnen') # Name des Fensters
 
+button_next = tkinter.Button(window, text='>')
+button_next.pack()
+
 window.mainloop() # Nötig um Fenster zu öffnen
